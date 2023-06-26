baff02f98f3564d087ff54a84e525f31543433ec
Rafael Bustillo <rafa.bustillo.brizuela@gmail.com>
2023-06-26 08:33:03 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
